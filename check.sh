489e028d10c4c13fb50b6df010e5c0f32d38e886
Drew Csillag <drew@thecsillags.com>
2025-01-31 13:10:06 -0500

    consolidated checking

--- check.sh
+++ check.sh
@@ -1,3 +1,9 @@
 black notepaper*.py
 ruff check notepaper*.py
 python -m unittest notepaper_test
+python notepaper.py 2025-01-01 8
+for i in *.svg
+do
+  diff -u $i exemplar/$i
+
+done
--- makecheck.sh
+++
@@ -1,6 +0,0 @@
-python notepaper.py 2025-01-01 8
-for i in *.svg
-do
-  diff -u $i exemplar/$i
-
-done
